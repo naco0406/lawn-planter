83aa5a79b9f19666febc74253c627901ed12d7ea
naco0406 <young0406@kaist.ac.kr>
2025-01-03 17:52:08 +0900

    chore: 개발을 위해 eslint 무시

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,14 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  // 개발중일 때만 eslint 무시
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off", // 사용하지 않는 변수 경고 끄기
+      "react/no-unescaped-entities": "off", // 추가로 따옴표 관련 경고도 끄기
+      "@typescript-eslint/no-explicit-any": "off" // any 타입 관련 경고도 끄기
+    }
+  }
 ];
 
 export default eslintConfig;
