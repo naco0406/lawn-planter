e39265cdea3f37572f67fa0f6079f4615724e2af
naco0406 <young0406@kaist.ac.kr>
2025-01-03 17:53:37 +0900

    chore: 개발을 위해 eslint 무시

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,8 +15,11 @@ const eslintConfig = [
   {
     rules: {
       "@typescript-eslint/no-unused-vars": "off", // 사용하지 않는 변수 경고 끄기
-      "react/no-unescaped-entities": "off", // 추가로 따옴표 관련 경고도 끄기
-      "@typescript-eslint/no-explicit-any": "off" // any 타입 관련 경고도 끄기
+      "react/no-unescaped-entities": "off", // 따옴표 관련 경고 끄기
+      "@typescript-eslint/no-explicit-any": "off", // any 타입 관련 경고 끄기
+      "@typescript-eslint/ban-ts-comment": "off", // ts-expect-error 관련 경고 끄기
+      "prefer-const": "off", // let을 const로 바꾸라는 경고 끄기
+      "@next/next/no-img-element": "off" // img 태그 대신 next/image를 사용하라는 경고 끄기
     }
   }
 ];
