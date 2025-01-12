71f770916a28ee463a17d650c007ea5a2cccd09e
naco0406 <young0406@kaist.ac.kr>
2025-01-12 09:41:37 +0900

    feat: PWA

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
+const withPWA = require("next-pwa");
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig = withPWA({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+}) satisfies NextConfig;
 
 export default nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-// src/app/layout.tsx
 import './globals.css'
 import { Inter } from 'next/font/google'
 import { getServerSession } from 'next-auth'
@@ -8,17 +7,40 @@ import { authOptions } from '@/lib/auth'
 
 const inter = Inter({ subsets: ['latin'] })
 
+export const metadata = {
+  title: 'Lawn Diary',
+  description: '매일 일기를 쓰고 깃허브 잔디를 가꿔보세요',
+  manifest: '/manifest.json',
+  icons: {
+    apple: [
+      { url: '/lawn_logo.png' },
+    ],
+  },
+  themeColor: '#ffffff',
+}
+
 export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   headers();
-  // const session = await getServerSession();
   const session = await getServerSession(authOptions);
 
   return (
     <html lang="ko">
+      <head>
+        <meta name="application-name" content="Lawn Diary" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="Lawn Diary" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="theme-color" content="#ffffff" />
+
+        <link rel="apple-touch-icon" href="/lawn_logo.png" />
+        <link rel="manifest" href="/manifest.json" />
+      </head>
       <body className={inter.className}>
         <SessionProvider session={session}>
           {children}
@@ -26,9 +48,4 @@ export default async function RootLayout({
       </body>
     </html>
   )
-}
-
-export const metadata = {
-  title: 'Lawn Diary',
-  description: '매일 일기를 쓰고 깃허브 잔디를 가꿔보세요',
 }
